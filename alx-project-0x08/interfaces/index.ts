585ea3eded81f32108aac4b2deda03af2f8c79a9
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-17 11:23:45 +0200

    feat: add GenerateImageProps interface and ImageProps type for  image component props

--- alx-project-0x08/interfaces/index.ts
+++ alx-project-0x08/interfaces/index.ts
@@ -3,3 +3,13 @@ import * as React from "react";
 export interface ReactComponentProps {
   children: React.ReactNode;
 }
+
+export interface GenerateImageProps {
+  imageUrl: string;
+  prompt: string;
+  width?: string;
+  height?: string;
+  action: (imagePath: string) => void
+}
+
+export type ImageProps = Pick<GenerateImageProps, "imageUrl" | "prompt">
