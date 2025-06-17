53d1c05b2cc166443f209e9f16f9431fa01c1699
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-17 13:57:41 +0200

    feat: handle API response and return generated image or fallback placeholder

--- alx-project-0x09/pages/api/generate-image.ts
+++ alx-project-0x09/pages/api/generate-image.ts
@@ -1,6 +1,7 @@
 import { HEIGHT, WIDTH } from "@/constants";
 import { RequestProps } from "@/interfaces";
 import { NextApiRequest, NextApiResponse } from "next";
+import { json } from "stream/consumers";
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   const gbtApiKey = process.env.NEXT_PUBLIC_GBT_API_KEY;
@@ -27,6 +28,18 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
         "x-rapidapi-host": "chatgpt-42.p.rapidapi.com",
         "Content-Type": "application/json",
       },
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch from DALLE");
+    }
+
+    const data = await res.json();
+
+    return response.status(200).json({
+      messsage:
+        data?.generated_image ||
+        "https://via.placeholder.com/600x400?text=Generated+Image",
     });
   } catch (error) {}
 };
