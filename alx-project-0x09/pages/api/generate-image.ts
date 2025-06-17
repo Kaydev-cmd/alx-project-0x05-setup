671ddab976b4b2d0e78a8982e1b795cb170c9bcb
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-17 13:54:15 +0200

    feat: implement GBT image generation API call with prompt and dimensions

--- alx-project-0x09/pages/api/generate-image.ts
+++ alx-project-0x09/pages/api/generate-image.ts
@@ -5,4 +5,28 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   const gbtApiKey = process.env.NEXT_PUBLIC_GBT_API_KEY;
   const gbtUrl = "https://chatgpt-42.p.rapidapi.com/texttoimage";
+
+  if (!gbtApiKey || !gbtUrl) {
+    return response
+      .status(500)
+      .json({ error: "API key or URL is missing in environment variables" });
+  }
+
+  try {
+    const { prompt }: RequestProps = request.body;
+
+    const res = await fetch(gbtUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        text: prompt,
+        width: WIDTH,
+        height: HEIGHT,
+      }),
+      headers: {
+        "x-rapidapi-key": gbtApiKey.trim(),
+        "x-rapidapi-host": "chatgpt-42.p.rapidapi.com",
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {}
 };
