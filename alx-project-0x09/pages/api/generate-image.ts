cba5b21919d52c798fcaa24e20f7514885451e35
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-17 13:47:09 +0200

    feat: initialize image generation API handler with GBT endpoint and API Key

--- alx-project-0x09/pages/api/generate-image.ts
+++ alx-project-0x09/pages/api/generate-image.ts
@@ -1,3 +1,8 @@
 import { HEIGHT, WIDTH } from "@/constants";
 import { RequestProps } from "@/interfaces";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  const gbtApiKey = process.env.NEXT_PUBLIC_GBT_API_KEY;
+  const gbtUrl = "https://chatgpt-42.p.rapidapi.com/texttoimage";
+};
