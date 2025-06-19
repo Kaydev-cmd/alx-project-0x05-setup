fc56d2f6fe07bf79c70462918ffc65c86fde5796
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-19 13:58:50 +0200

    feat: create reusable useFetchData hook with generic typing and image state management

---
+++ alx-project-0x12/hooks/useFetchData.ts
@@ -0,0 +1,9 @@
+import { ImageProps } from "@/interfaces";
+import { useState } from "react";
+
+const useFetchData = <T, R>() => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [responseData, setResponseData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
+};
